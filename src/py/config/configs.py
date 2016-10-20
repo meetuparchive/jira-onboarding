978f3b0e7eb0ec532a5bfe362459cb9e61032a7f
Jake <jake@meetup.com>
2016-10-20 11:58:12 -0400

    break out validation

--- src/py/config/configs.py
+++ src/py/config/configs.py
@@ -1,53 +1,9 @@
 from __future__ import with_statement
-from jira import JIRA
-import json, sys, traceback, yaml
+from validation.validation import validate_config
 
-# TODO move to own place
-def check_jira_server(server):
-  try:
-    return JIRA(server)
-  except:
-    return None
+import json, yaml
 
-def validate_config(config_json):
-  try:
-    assert "server" in config_json # config must have server specified
-    assert check_jira_server(config_json["server"]) # specified server must be valid
-   
-    assert "name" in config_json # config must have a name specified
-    assert config_json["name"] # name must be non-empty
-
-    assert "project_key" in config_json # config must have a project key specified
-    assert config_json["project_key"] # project key must be non-empty
-
-    assert "epic_fields" in config_json # config must have epic field block specified
-    epic_json = config_json["epic_fields"]
-    assert "summary" in epic_json # config must have an epic summary specified
-    assert epic_json["summary"] # epic summary must be non-empty
-
-    assert "description" in epic_json # config must have an epic description specified
-    assert epic_json["description"] # epic_description must be non-empty
-
-    assert "issues_to_create" in config_json # config must have a list of issues specified
-    assert len(config_json["issues_to_create"]) > 0 # list of issues must be non-empty
-
-    for issue_json in config_json["issues_to_create"]:
-      assert "summary" in issue_json # each issue must have a summary specified
-      assert "description" in issue_json # each issue must have a description specified
-      if "assignee" in issue_json:
-        assert "name" in issue_json["assignee"] # if assignee is specified, must have a field called 'name' insde
-        
-
-  except AssertionError:
-    print "Config did not pass validation:"
-    _, _, tb = sys.exc_info()
-    traceback.print_tb(tb) # Fixed format
-    tb_info = traceback.extract_tb(tb)
-    filename, line, func, text = tb_info[-1]
-    return False
-  return True
-
-
+## Load a config from filepath
 def load_config(path_to_config):
   loaded = None
   with open(path_to_config, "r") as f:
